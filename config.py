b617babd8d84d3b9f63c941122517119f8fe7953
Владислав Кошелев <testvlad59@gmail.com>
2024-10-28 21:47:13 +0300

    исправлены ошибки

--- config.py
+++ config.py
@@ -8,9 +8,3 @@ LOGS_DIR = os.path.join(ROOT_DIR, "logs")
 
 # Директория для файлов с данными
 DATA_DIR = os.path.join(ROOT_DIR, "data")
-
-# Создание каталога для логов, если он не существует
-if not os.path.exists(LOGS_DIR):
-    os.makedirs(LOGS_DIR)
-
-UTILS_LOGS = os.path.join(LOGS_DIR, "utils.log")
